408d3097ef462d4bd5f62daf77edba3a92012983
章文泽 <zwz614371723@icloud.com>
2024-02-05 15:17:55 +0300

    day3

---
+++ springboot-auto-config/src/main/java/com/ventsze/springbootautoconfig/SpringbootAutoConfigApplication.java
@@ -0,0 +1,15 @@
+package com.ventsze.springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootAutoConfigApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringbootAutoConfigApplication.class, args);
+        System.out.println(context.getBean("dispatcherServlet"));
+    }
+}
